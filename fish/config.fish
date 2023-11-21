ff2726e7fca82478423305c2653756e3aece5800
F@t.sk4l <57485491+F4tsk4l@users.noreply.github.com>
2023-11-21 15:49:05 +0300

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
-if status is-interactive
-and not set -q TMUX
-    exec tmux 
-end
+#if status is-interactive
+#and not set -q TMUX
+#    exec tmux 
+#end
 function st_docker
 sudo systemctl start docker.socket && sudo systemctl start docker
 end
@@ -12,26 +12,14 @@ alias ls "eza --icons"
 alias ll "eza -al --icons"
 alias l  "eza -a --icons"
 alias vim "nvim"
+alias rmr "rm -rf"
+alias cls "clear"
+alias cdld "cd '/run/media/sc0rp/Local Disk/'"
+alias yayi "yay -S --cleanafter"
 
-function cls
-clear
-end
-
-#function dl
-#cd /run/media/Cr33p3r/Local Disk/
-#end
-
-alias ld "cd '/run/media/cr33p3r/Local\ Disk/'"
-function yayi
-yay -S --cleanafter
-end
-
-#set -g PATH "$HOME/emmcdl:$PATH"
-#set -g PATH "$HOME/.local/bin:$PATH"
-set -g PATH "$HOME/Downloads/Compressed/jcryptool:$PATH"
-#set -g dlp "/run/media/Cr33p3r/Local Disk/"
+set -g PATH "$HOME/.local/bin:$PATH"
 set fish_greeting ""
-export VISUAL=vim
+export VISUAL=nvim
 export EDITOR="$VISUAL"
 
 #if [-z "$TMUX"]; then tmux; fi
