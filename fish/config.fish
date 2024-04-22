06e0371b04964f2c21c0595f5ed771c6321c401b
F@t.sk4l <57485491+F4tsk4l@users.noreply.github.com>
2024-04-22 20:57:48 +0300

    before artix

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,17 @@
-#if status is-interactive
-#and not set -q TMUX
-#    exec tmux 
-#end
+if status is-interactive
+    if not set -q TMUX
+        set -g TMUX tmux new -d -s base
+        eval $TMUX
+        tmux a -d -t base
+    end
+end
 function st_docker
 sudo systemctl start docker.socket && sudo systemctl start docker
 end
 function sp_docker
 sudo systemctl stop docker.socket && sudo systemctl stop docker
 end
+alias cl "xsel -i --clipboard"
 alias ls "eza --icons"
 alias ll "eza -al --icons"
 alias l  "eza -a --icons"
@@ -24,6 +28,4 @@ set -g PATH "$HOME/.local/bin:$PATH"
 set fish_greeting ""
 export VISUAL=nvim
 export EDITOR="$VISUAL"
-export LD_PRELOAD=/usr/lib/libgtk3-nocsd.so.0
-#export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgtk3-nocsd.so.0
-#if [-z "$TMUX"]; then tmux; fi
+
