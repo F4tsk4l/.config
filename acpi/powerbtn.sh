b39e67ded54dd64bebe00ccb0f5f395a8f5cbd5b
F@t.sk4l <57485491+F4tsk4l@users.noreply.github.com>
2025-09-01 20:21:35 +0000

    Update powerbtn.sh

--- acpi/powerbtn.sh
+++ acpi/powerbtn.sh
@@ -1,9 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-USER="Username"
-USER_HOME="/home/$USER"
-export DISPLAY=:0
-export XAUTHORITY="$USER_HOME/.Xauthority"
+# Detect username of active X session
+USER=$(loginctl list-sessions 2>/dev/null | awk '/tty[0-9]/ {print $3; exit}')
+[ -z "$USER" ] && USER=$(logname)
 
-# Start powermenu within user's X session so it behaves like a keybinding-triggered launch
-setsid su -l "$USER" -c "DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY $USER_HOME/.local/bin/powermenu" &
+HOME_DIR=$(eval echo "~$USER")
+
+# Detect active X display
+DISPLAY_NUM=$(w -hs | awk '/tty[0-9]/ && /X/ {print $3; exit}')
+[ -z "$DISPLAY_NUM" ] && DISPLAY_NUM=":0"   # fallback
+
+# Detect XAUTHORITY
+XAUTHORITY=$(ps -u "$USER" -o pid= | \
+    xargs -I{} grep -z '^' /proc/{}/environ 2>/dev/null | \
+    tr '\0' '\n' | awk -F= '/XAUTHORITY/ {print $2; exit}')
+[ -z "$XAUTHORITY" ] && XAUTHORITY="$HOME_DIR/.Xauthority"
+
+export DISPLAY="$DISPLAY_NUM"
+export XAUTHORITY="$XAUTHORITY"
+
+# Launch powermenu in user session
+setsid su -l "$USER" -c "DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY $HOME_DIR/.local/bin/powermenu" &
