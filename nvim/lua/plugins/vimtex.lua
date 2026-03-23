19427c1917d38c2deb296c7af35aef001ab9cc19
F4tsk4l <enockkip11@gmail.com>
2026-03-23 21:42:14 +0300

    Nvim vimtex adjustments

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -2,10 +2,17 @@ return {
   "lervag/vimtex",
   lazy = false, -- lazy-loading will disable inverse search
   config = function()
-    -- vim.g.maplocalleader = ","
+    vim.g.maplocalleader = ","
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-    -- vim.g.vimtex_compiler_method = "latexrun"
-    vim.g.vimtex_compiler_engine = "xelatex"
+    --vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        "-pdflatex",
+        "-interaction=nonstopmode",
+        "-synctex=1",
+      },
+    }
     vim.g.vimtex_view_method = "zathura"
   end,
   keys = {
